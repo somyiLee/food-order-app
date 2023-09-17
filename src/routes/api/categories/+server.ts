d5bed448d0a63f2a7c775217f5bf846d0b393c28
이솜이 <122191856+somyiLee@users.noreply.github.com>
2023-09-18 00:20:14 +0900

    feat: add load files

--- src/routes/api/categories/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { categories } from '$lib/data/index';
-
-export function GET() {
-	return json(categories);
-}
--- src/routes/api/products/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { products } from '$lib/data/index';
-
-export function GET() {
-	return json(products);
-}
